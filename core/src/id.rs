96bb48ab400eda377224e5a7fb4f36654477f4c4
Conrad Parker <rayradjr@gmail.com>
2022-10-06 19:52:01 -0700

    Add documentation for new ID abstractions

--- core/src/id.rs
+++ core/src/id.rs
@@ -4,11 +4,14 @@ use std::{
     ops::{Deref, Index},
 };
 
+/// An ID type that wraps a `usize`. Used to assign unique IDs to objects based on their index in an
+/// array.
 pub trait Id {
     fn from_raw(raw: usize) -> Self;
     fn raw(&self) -> usize;
 }
 
+/// An array of `I` IDs for `T` values. Similar to a `[T]`, but indexed by `I`s.
 #[derive(Debug)]
 #[repr(transparent)]
 pub struct Ids<I, T> {
@@ -27,14 +30,17 @@ impl<I, T> Ids<I, T> {
         unsafe { Box::from_raw(transmute(Box::into_raw(raw))) }
     }
 
+    /// As `[T]::is_empty()`.
     pub fn is_empty(&self) -> bool {
         self.slice.is_empty()
     }
 
+    /// As `[T]::len()`.
     pub fn len(&self) -> usize {
         self.slice.len()
     }
 
+    /// Copies the slice into an [`IdGen`].
     pub fn to_gen(&self) -> IdGen<I, T>
     where
         T: Clone,
@@ -42,6 +48,7 @@ impl<I, T> Ids<I, T> {
         IdGen::from_raw(self.slice.to_vec())
     }
 
+    /// Consumes a boxed slice and converts it to an [`IdGen`].
     pub fn into_gen(self: Box<Ids<I, T>>) -> IdGen<I, T> {
         // Safe because of #[repr(transparent)].
         IdGen::from_raw(unsafe { Box::<[T]>::from_raw(transmute(Box::into_raw(self))) }.into_vec())
@@ -52,10 +59,12 @@ impl<I, T> Ids<I, T>
 where
     I: Id,
 {
+    /// As `[T]::get(usize)`, but requires an `I`.
     pub fn get(&self, index: I) -> Option<&T> {
         self.slice.get(index.raw())
     }
 
+    /// As `[T]::get__unchecked(usize)`, but requires an `I`.
     pub unsafe fn get_unchecked(&self, index: I) -> &T {
         self.slice.get_unchecked(index.raw())
     }
@@ -78,6 +87,7 @@ where
     }
 }
 
+/// An ID generator for `T` values indexed by `I`. Wraps a `Vec<T>` internally.
 #[derive(Debug)]
 pub struct IdGen<I, T> {
     vec: Vec<T>,
@@ -92,36 +102,44 @@ impl<I, T> IdGen<I, T> {
         }
     }
 
+    /// As `Vec<T>::new()`.
     pub fn new() -> IdGen<I, T> {
         IdGen::from_raw(Vec::new())
     }
 
+    /// As `Vec<T>::with_capacity()`.
     pub fn with_capacity(capacity: usize) -> IdGen<I, T> {
         IdGen::from_raw(Vec::with_capacity(capacity))
     }
 
+    /// As `Vec<T>::capacity()`.
     pub fn capacity(&self) -> usize {
         self.vec.capacity()
     }
 
-    pub fn into_boxed_ids(self) -> Box<Ids<I, T>> {
-        Ids::from_boxed_raw(self.vec.into_boxed_slice())
-    }
-
+    /// As `Vec<T>::is_empty()`.
     pub fn is_empty(&self) -> bool {
         self.vec.is_empty()
     }
 
+    /// As `Vec<T>::len()`.
     pub fn len(&self) -> usize {
         self.vec.len()
     }
 
+    /// As `Vec<T>::reserve()`.
     pub fn reserve(&mut self, additional: usize) {
         self.vec.reserve(additional)
     }
 
+    /// As `Vec<T>::reserve_exact()`.
     pub fn reserve_exact(&mut self, additional: usize) {
         self.vec.reserve_exact(additional)
+    }
+
+    /// Consumes the `IdGen` and converts it into a `Box<Ids<I, T>>`.
+    pub fn into_boxed_ids(self) -> Box<Ids<I, T>> {
+        Ids::from_boxed_raw(self.vec.into_boxed_slice())
     }
 }
 
@@ -129,6 +147,7 @@ impl<I, T> IdGen<I, T>
 where
     I: Id,
 {
+    /// As `Vec<T>::push()`, but returns the ID of the new element.
     pub fn push(&mut self, value: T) -> I {
         let id = I::from_raw(self.len());
         self.vec.push(value);
