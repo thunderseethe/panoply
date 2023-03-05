89db747ec5da565c8020f11f72f4d7adf290063a
thunderseethe <thunderseethe.dev@gmail.com>
2023-03-05 19:52:34 +0000

    Add salsa as dependency of `analysis`

--- analysis/src/lib.rs
+++ analysis/src/lib.rs
@@ -6,3 +6,8 @@ pub mod names;
 pub mod ops;
 pub mod resolve;
 pub mod top_level;
+
+#[salsa::jar(db = Db)]
+pub struct Jar();
+pub trait Db: salsa::DbWithJar<Jar> + aiahr_core::Db {}
+impl<DB> Db for DB where DB: salsa::DbWithJar<Jar> + aiahr_core::Db {}
