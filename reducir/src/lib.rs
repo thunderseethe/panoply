9c613879789f47b8fb9151eb19a4eb6253bf754b
thunderseethe <thunderseethe.dev@gmail.com>
2023-07-23 03:57:35 +0000

    Move zip non consuming to it's own module

--- reducir/src/lib.rs
+++ reducir/src/lib.rs
@@ -91,61 +91,8 @@ use crate::ty::ReducIrVarTy;
 
 use self::ty::{Kind, MkReducIrTy, ReducIrTy, ReducIrTyApp};
 
-/// A zip that does not own it's iterators and does not consume elements when only one iterator has
-/// elements remaining.
-struct LentZip<'a, A, B>
-where
-    A: Iterator,
-    B: Iterator,
-{
-    a_iter: &'a mut std::iter::Peekable<A>,
-    b_iter: &'a mut std::iter::Peekable<B>,
-}
-impl<'a, A, B> Iterator for LentZip<'a, A, B>
-where
-    A: Iterator,
-    B: Iterator,
-{
-    type Item = (A::Item, B::Item);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        match (self.a_iter.peek(), self.b_iter.peek()) {
-            (Some(_), Some(_)) => Some((self.a_iter.next().unwrap(), self.b_iter.next().unwrap())),
-            _ => None,
-        }
-    }
-}
-
-/// Zips two iterators without consuming them.
-/// This allows for iterating over the overlap of two iterators, and then consuming whatever
-/// remains in the two iterators separately after the fact.
-trait ZipNonConsuming {
-    type Iter: Iterator;
-
-    fn zip_non_consuming<'a, J>(
-        &'a mut self,
-        other: &'a mut std::iter::Peekable<J>,
-    ) -> LentZip<'a, Self::Iter, J>
-    where
-        J: Sized + Iterator;
-}
-impl<I: Iterator> ZipNonConsuming for std::iter::Peekable<I> {
-    type Iter = I;
-
-    fn zip_non_consuming<'a, J>(
-        &'a mut self,
-        other: &'a mut std::iter::Peekable<J>,
-    ) -> LentZip<'a, I, J>
-    where
-        Self: Sized + Iterator,
-        J: Sized + Iterator,
-    {
-        LentZip {
-            a_iter: self,
-            b_iter: other,
-        }
-    }
-}
+mod zip_non_consuming;
+use zip_non_consuming::ZipNonConsuming;
 
 /// A ReducIr node
 /// `ReducIr` is much more explicit than `Term`. It is based on System F with some modest
---
+++ reducir/src/zip_non_consuming.rs
@@ -0,0 +1,55 @@
+/// A zip that does not own it's iterators and does not consume elements when only one iterator has
+/// elements remaining.
+pub(crate) struct LentZip<'a, A, B>
+where
+    A: Iterator,
+    B: Iterator,
+{
+    a_iter: &'a mut std::iter::Peekable<A>,
+    b_iter: &'a mut std::iter::Peekable<B>,
+}
+impl<'a, A, B> Iterator for LentZip<'a, A, B>
+where
+    A: Iterator,
+    B: Iterator,
+{
+    type Item = (A::Item, B::Item);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match (self.a_iter.peek(), self.b_iter.peek()) {
+            (Some(_), Some(_)) => Some((self.a_iter.next().unwrap(), self.b_iter.next().unwrap())),
+            _ => None,
+        }
+    }
+}
+
+/// Zips two iterators without consuming them.
+/// This allows for iterating over the overlap of two iterators, and then consuming whatever
+/// remains in the two iterators separately after the fact.
+pub(crate) trait ZipNonConsuming {
+    type Iter: Iterator;
+
+    fn zip_non_consuming<'a, J>(
+        &'a mut self,
+        other: &'a mut std::iter::Peekable<J>,
+    ) -> LentZip<'a, Self::Iter, J>
+    where
+        J: Sized + Iterator;
+}
+impl<I: Iterator> ZipNonConsuming for std::iter::Peekable<I> {
+    type Iter = I;
+
+    fn zip_non_consuming<'a, J>(
+        &'a mut self,
+        other: &'a mut std::iter::Peekable<J>,
+    ) -> LentZip<'a, I, J>
+    where
+        Self: Sized + Iterator,
+        J: Sized + Iterator,
+    {
+        LentZip {
+            a_iter: self,
+            b_iter: other,
+        }
+    }
+}
