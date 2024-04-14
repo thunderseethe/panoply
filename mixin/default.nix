06d0913e0214433fc5a60ed23a541aff104dce5f
thunderseethe <thunderseethe.dev@gmail.com>
2024-04-14 03:45:15 +0000

    Remove mixin from this repo

--- mixin/default.nix
+++
@@ -1,8 +0,0 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc982" }:
-nixpkgs.haskell.packages.${compiler}.developPackage {
-  root = ./.;
-  withHoogle = true;
-  modifier = drv:
-    nixpkgs.haskell.lib.addBuildTools drv (with nixpkgs.haskellPackages;
-      [ cabal-install hoogle ghc haskell-language-server ]);
-}
